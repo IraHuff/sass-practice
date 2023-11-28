72b679057c902a76333cebb1919b81be3f63ea26
Ira Huff <ira.m.huff@gmail.com>
2023-11-28 11:08:11 -0600

    partials and use

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('src/css/main.sass')
+    return src('src/css/**/*.sass')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['src/css/main.sass'], buildStyles)
+    watch(['src/css/**/*.sass'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
